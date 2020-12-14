fe8a1f606b258179aec832b9853528e8e90020fa
Obihan98 <73716961+Obihan98@users.noreply.github.com>
2020-12-13 17:13:53 -0800

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+import random
+
+def play():
+    human = input("'r' for rock, 'p' for paper, 's' for scissors: ")
+    computer = random.choice(["r", "p", "s"])
+
+    if human == computer:
+        return "It's a tie."
+
+    if is_win(human, computer):
+        return "You won!"
+
+    return "You lost!"
+
+def is_win(human, computer):
+    if (human == "r" and computer == "s") or (human == "s" and computer == "p") \
+        or (human == "p" and computer == "r"):
+        return True
+
+print(play())
